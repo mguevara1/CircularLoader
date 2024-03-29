f30b60ea55f7a56e7d7053ffee7f84b4cb0559ee
Marco Guevara <marco.algv@gmail.com>
2024-03-28 18:18:24 -0600

    Download data task and real time progress update

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -7,22 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
 
     let shapeLayer = CAShapeLayer()
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let urlString = "https://www.apple.com/105/media/us/iphone-x/2017/01df5b43-28e4-4848-bf20-490c34a926a7/overview/modal/dual_cameras_pro_mode/large_2x.mp4"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let center = view.center
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+        
         let trackLayer = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         
         trackLayer.path = circularPath.cgPath
         trackLayer.strokeColor = UIColor.lightGray.cgColor
         trackLayer.lineWidth = 10
         trackLayer.lineCap = .round
         trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.position = view.center
         
         view.layer.addSublayer(trackLayer)
         
@@ -31,6 +43,8 @@ class ViewController: UIViewController {
         shapeLayer.lineWidth = 10
         shapeLayer.lineCap = .round
         shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.position = view.center
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
         shapeLayer.strokeEnd = 0
         
         view.layer.addSublayer(shapeLayer)
@@ -38,7 +52,17 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
 
-    @objc private func handleTap() {
+    private func beginDownloadingFile() {
+        shapeLayer.strokeEnd = 0
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: operationQueue)
+        
+        guard let url = URL(string: urlString) else { return }
+        let downloadTask = urlSession.downloadTask(with: url)
+        downloadTask.resume()
+    }
+    
+    fileprivate func animateCircle() {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         
         basicAnimation.toValue = 1
@@ -48,6 +72,24 @@ class ViewController: UIViewController {
         
         shapeLayer.add(basicAnimation, forKey: "circleBorderAnimation")
     }
+    
+    @objc private func handleTap() {
+        beginDownloadingFile()
+    }
 
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("Finished downloading file")
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        
+        DispatchQueue.main.async {
+            self.percentageLabel.text = "\(Int(percentage * 100))%"
+            self.shapeLayer.strokeEnd = CGFloat(percentage)
+        }
+        
+        print(percentage)
+    }
 }
 
